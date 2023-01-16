b7d8795d36ba20ab479fd3126167710b2a6197b1
Bo <boprsoft.dev@gmail.com>
2023-01-16 19:47:20 +0300

    add ini parser

--- util/util.psm1
+++ util/util.psm1
@@ -55,7 +55,41 @@ function Get-NormalizedFilename
     return ((($name -replace "[\\/:|<>]", "¦") -replace "[*]", "·") -replace "[?]", "$") -replace "[\`"]", "'"
 }
 
+function Get-IniContent
+{
+    param(
+        [String]$filename
+    )
+
+    $map = @{ }
+    $section = "no-section"
+    $map[$section] = @{ }
+    switch -regex -file $filename
+    {
+        "^\s*[;#](.*)$" {
+            Write-Debug $matches[1]
+        }
+        "^\s*\[(.+)\]\s*$" {
+            $section = $matches[1]
+            $map[$section] = @{ }
+        }
+        "^\s*(.+)\s*=\s*(.*)$" {
+            $name, $value = $matches[1..2]
+            $map[$section][$name] = $value
+        }
+        default {
+            if (-not$map[$section]["raw"])
+            {
+                $map[$section]["raw"] = @()
+            }
+            $map[$section]["raw"] += $_
+        }
+    }
+    $map
+}
+
 Export-ModuleMember -Function Get-Capitalized
 Export-ModuleMember -Function Set-ConsoleEncoding
 Export-ModuleMember -Function Set-Extension
 Export-ModuleMember -Function Get-NormalizedFilename
+Export-ModuleMember -Function Get-IniContent
