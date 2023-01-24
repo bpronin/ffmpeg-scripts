8c40fc63882bbb1415b0ba053870b2ce20c73a50
Bo <boprsoft.dev@gmail.com>
2023-01-25 00:21:46 +0300

    update m4a extractor

--- extract-m4a.ps1
+++ extract-m4a.ps1
@@ -1,28 +1,20 @@
-Import-Module -Name $PSScriptRoot\util
-Import-Module -Name $PSScriptRoot\ffmpeg
-
 $include = @("*.mkv", "*.mp4")
-$tasks = @()
 
 $args | ForEach-Object {
     Get-ChildItem -Path $_ -Include $include -Recurse | ForEach-Object{
-        $tasks += @{
-            source = $_
-            target = Set-Extension -File $_ -Extension ".m4a"
-            metadata = @{
-                title = $_.BaseName
-            }
-        }
-    }
-}
+        Start-ThreadJob -ScriptBlock {
+                $source = $using:_
+                $target = Join-Path $source.DirectoryName "$($source.BaseName).m4a"
+                $metadata = @{
+                    title = $source.BaseName
+                }
 
-$tasks | ForEach-Object {
-    Start-ThreadJob -ScriptBlock {
-        $task = $using:_
-        Write-Host "Extracting track: $( $task.target )"
-        Import-Module -Name $using:PSScriptRoot\ffmpeg
-        Copy-Audio -source $task.source -target $task.target -metadata $task.metadata
-    } -StreamingHost $Host -ThrottleLimit 50 -ArgumentList $_ | Receive-Job
+                Write-Host "Extracting track: $target"
+
+                Import-Module -Name $using:PSScriptRoot\ffmpeg
+                Copy-Audio -source $source -target $target -metadata $metadata
+        } -StreamingHost $Host -ThrottleLimit 50 -ArgumentList $_ | Receive-Job
+    }
 }
 
 Get-Job | Wait-Job | Out-Null
