4c282bb756b2ac6b7b76aa0e196af3bd47376d4e
bpronin <boris.i.pronin@gmail.com>
2026-04-01 16:58:55 +0300

    updates

--- book-m4b.ps1
+++ book-m4b.ps1
@@ -3,12 +3,15 @@ param(
     # [System.IO.DirectoryInfo] $InputPath = "C:\Temp\t",
     # [System.IO.FileInfo] $FfmpegHome = "c:\opt\media\ffmpeg\bin"
 )
-
-[System.IO.DirectoryInfo] $InputPath = "C:\Temp\t"
+$InputPath = "C:\Temp\~\temp"
 $FfmpegHome = "c:\opt\media\ffmpeg\bin"
+$ImagicHome = "C:\Opt\imagick"
 
-$ffprobe = "$FfmpegHome\ffprobe"
-$ffmpeg = "$FfmpegHome\ffmpeg"
+$ffprobe = Join-Path $FfmpegHome "ffprobe.exe"
+$ffmpeg = Join-Path $FfmpegHome "ffmpeg.exe"
+$imagick = Join-Path $ImagicHome "magick.exe"
+# $ff_loglevel = "error"
+$ff_loglevel = "warning"
 
 function ReadFileMetadata {
     param (
@@ -37,7 +40,7 @@ function PrepareTempPath {
         [System.IO.DirectoryInfo] $InputPath
     )
 
-    $tempPath = "$InputPath\~out"
+    $tempPath = "$InputPath\~tmp"
     Remove-Item -Path $tempPath -Recurse -Confirm:$false -Force -ErrorAction SilentlyContinue
     New-Item -Path $tempPath -ItemType Directory -Force | Out-Null
     
@@ -75,19 +78,25 @@ function AttachCover {
         [System.IO.FileInfo] $InputFile
     )
     
-    ### Extract picture ###
     $coverFile = Join-Path $InputFile.Directory "~cover.jpg"
-    & $ffmpeg -i $CoverSource -c:v copy -an $coverFile -y #-loglevel error 
+
+    ### Extract picture ###
+    # & $ffmpeg -i $CoverSource -c:v copy -an $coverFile -y -loglevel $ff_loglevel 
+    & $ffmpeg -i $CoverSource -map 0:v -update 1 -c copy $coverFile -y -loglevel $ff_loglevel 
 
     if (Test-Path -Path $coverFile -PathType Leaf) {
+
+        ### Convert image ###
+        & $imagick mogrify -resize 400x400 -quality 80 -format jpg $coverFile 
+        
         $outputFile = $InputFile
 
         $tempFile = Join-Path $InputFile.Directory "~temp$($InputFile.Extension)"
         Rename-Item -Path $InputFile -NewName $tempFile
 
         ### Attach picture ###
-        & $ffmpeg -i $tempFile -i $coverFile -map 0:a -map 1:v -c copy -disposition:v:0 attached_pic $outputFile -y # -loglevel error
-    
+        & $ffmpeg -i $tempFile -i $coverFile -map 0:a -map 1:v -c copy -disposition:v:0 attached_pic $outputFile -y -loglevel $ff_loglevel
+        
         Remove-Item $tempFile, $coverFile
     }
 }
@@ -158,34 +167,40 @@ function JoinChapters {
     Out-File -FilePath $metadataFile -InputObject $metadata -Encoding utf8NoBOM
     Out-File -FilePath $listFile -InputObject $files -Encoding utf8NoBOM
     
-    & $ffmpeg -f concat -safe 0 -i $listFile -i $metadataFile -map_metadata 1 -map 0:a -c copy $OutputFile -y # -loglevel error
+    & $ffmpeg -f concat -safe 0 -i $listFile -i $metadataFile -map_metadata 1 -map 0:a -c copy $OutputFile -y -loglevel $ff_loglevel
     
     Remove-Item $metadataFile, $listFile
 }
 
-function ConvertInputFiles {
+function ConvertFiles {
     param (
         [System.IO.DirectoryInfo] $InputPath,
         [System.IO.DirectoryInfo] $OutputPath
     )
 
-    $files = Get-ChildItem -Path "$InputPath\*" -Include "*.m4a"
-    foreach ($file in $files) {
-        Copy-Item -Path $file -Destination $OutputPath     
-    }
-    
-    $files = Get-ChildItem -Path "$InputPath\*" -Include "*.mp3"
-    foreach ($file in $files) {
-        $outputFile = Join-Path $OutputPath ($file.BaseName + ".m4a")
-        & $ffmpeg -i $file.FullName -c:a aac -q:a 2 $outputFile -y
-    }
-    
+    $files = Get-ChildItem -Path "$InputPath\*" -Include *.mp3, *.m4a -File
+    $files | Foreach-Object -ThrottleLimit 16 -Parallel {
+        $inputFile = $_
+        switch ($inputFile.Extension) {
+            ".m4a" {  
+                Copy-Item -Path $inputFile -Destination $using:OutputPath
+            }
+            ".mp3" {  
+                $outputFile = Join-Path $using:OutputPath ($inputFile.BaseName + ".m4a")
+                & $using:ffmpeg -i $inputFile -map 0:a -map 0:v -c:a aac -q:a 2 -c:v copy -disposition:v:0 attached_pic $outputFile `
+                    -y -loglevel $using:ff_loglevel
+            }
+            Default {
+                Write-Warning "Unsupported extension"
+            }
+        }
+    } 
 }
 
 ############## Script entry point ################
 
 $tempPath = PrepareTempPath -InputPath $InputPath
-ConvertInputFiles -InputPath $InputPath -OutputPath $tempPath
+ConvertFiles -InputPath $InputPath -OutputPath $tempPath
 $chapters = ReadChapters -InputPath $tempPath
 $outputFile = GetOutputFile -Tags $chapters[0].tags -InputFile $InputPath.BaseName -OutputPath $tempPath
 JoinChapters -Chapters $chapters -OutputFile $outputFile
